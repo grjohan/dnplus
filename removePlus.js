965fe923762eb85472e070bf4878fcbae4b92cc7
unknown <grjohan@student.chalmers.se>
2013-07-21 14:26:41 +0200

    Fix indentation from tabs to spaces

--- removePlus.js
+++ removePlus.js
@@ -1,34 +1,33 @@
 var temp,upLevels;
 if (window.location.hostname.indexOf("aftonbladet") != -1){
-	temp = "abPfxPremium";
-	upLevels = 2;
+    temp = "abPfxPremium";
+    upLevels = 2;
 } else {
-	temp = "premium"
-	upLevels = 1;
+    temp = "premium"
+    upLevels = 1;
 }
 var count = 0;
 removePlusFromTagName('*',temp);
 //removePlusFromTagName('img');
 
 function removePlusFromTagName (tagName,removeClass) {
-	var elems = document.getElementsByTagName(tagName);
-	var i;
-	for(i in elems){
-		var elem = elems[i];
-		if(elem.getAttribute)
-		{
-			var classValues = elem.getAttribute('class');
-			if(classValues !== null){
-				if(classValues.indexOf(removeClass) != -1){
-					var parent = elem;
-					for(var j = 0; j < upLevels ; j++){
-						parent = parent.parentNode;
-						if(j+1 === upLevels){
-							parent.parentNode.removeChild(parent);	
-						}
-					}
-				}
-			}
-		}
-	}
+    var elems = document.getElementsByTagName(tagName);
+    var i;
+    for(i in elems){
+        var elem = elems[i];
+        if(elem.getAttribute){
+            var classValues = elem.getAttribute('class');
+            if(classValues !== null){
+                if(classValues.indexOf(removeClass) != -1){
+                    var parent = elem;
+                    for(var j = 0; j < upLevels ; j++){
+                        parent = parent.parentNode;
+                        if(j+1 === upLevels){
+                            parent.parentNode.removeChild(parent);	
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
