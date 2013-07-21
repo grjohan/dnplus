54bafbd3840155955cfba056b5fddcc52bd8cdd9
unknown <grjohan@student.chalmers.se>
2013-07-21 12:04:27 +0200

    Add code for removing most of plus content(slow slow slow)

---
+++ removePlus.js
@@ -0,0 +1,21 @@
+removePlusFromTagName('a');
+removePlusFromTagName('img');
+
+
+function removePlusFromTagName (tagName) {
+	var elems = document.getElementsByTagName('tagName');
+	var i;
+	for(i in elems){
+		var elem = elems[i];
+		if(elem.getAttribute)
+		{
+			var classValues = elem.getAttribute('class');
+			if(classValues !== null){
+				if(classValues.indexOf('premium') != -1){
+					var parent = elem.parentNode;
+					parent.parentNode.removeChild(parent);	
+				}
+			}
+		}
+	}
+}
